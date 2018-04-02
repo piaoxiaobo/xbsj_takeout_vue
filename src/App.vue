5f28831c1e614c2e4ee492938dcdef8c685a0e86
piaoxiaobo <643209899@qq.com>
2018-04-02 10:39:36 +0800

    'update'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+    <div>
+      <router-view></router-view>
+      <FooterGuide></FooterGuide>
+    </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+      components:{
+        FooterGuide
+      }
+    }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Bianrongcheng on 2018
+ * 路由器模板
+ */
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import Msite from '../pages/Msite/Msite.vue';
+import Order from '../pages/Order/Order.vue';
+import Profile from '../pages/Profile/Profile.vue';
+import Search from '../pages/Search/Search.vue';
+
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+  routes: [
+    {
+      path: '/',
+      redirect: '/msite'
+    },
+    {
+      path: '/msite',
+      component: Msite,
+    },
+    {
+      path: '/search',
+      component: Search,
+    },
+    {
+      path: '/order',
+      component: Order,
+    },
+    {
+      path: '/profile',
+      component: Profile,
+    }
+  ]
+})

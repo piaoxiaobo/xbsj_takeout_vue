210c07a44bd2b7ed0b23164bbf289b3bb28be6ec
piaoxiaobo <643209899@qq.com>
2018-04-03 21:38:59 +0800

    ''update

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -4,40 +4,47 @@
       <div class="login_header">
         <h2 class="login_logo">小泊外卖</h2>
         <div class="login_header_title">
-          <a href="javascript:;" class="on">短信登录</a>
-          <a href="javascript:;" >密码登录</a>
+          <a href="javascript:;" :class="{on: loginWay}" @click="loginWay=true">短信登录</a>
+          <a href="javascript:;" :class="{on: !loginWay}" @click="loginWay=false">密码登录</a>
         </div>
       </div>
       <div class="login_content">
         <form>
-          <div class="on">
+          <div :class="{on: loginWay}">
             <section class="login_message">
-              <input type="tel" maxlength="11" placeholder="手机号">
-              <button disabled="disabled" class="get_verification" >获取验证码</button>
+              <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
+              <button :disabled="computeTime>0 || !rightPhone" class="get_verification"
+                      :class="{right_phone_number: rightPhone}" @click="sendCode">
+                {{computeTime ? `已发送(${computeTime}s)` : '获取验证码'}}
+              </button>
             </section>
             <section class="login_verification">
-              <input type="tel" maxlength="8" placeholder="验证码">
+              <input type="tel" maxlength="8" placeholder="验证码" v-model="code">
             </section>
             <section class="login_hint">
               温馨提示：未注册小泊外卖帐号的手机号，登录时将自动注册，且代表已同意
               <a href="javascript:;">《用户服务协议》</a>
             </section>
           </div>
-          <div>
+          <div :class="{on: !loginWay}">
             <section>
               <section class="login_message">
-                <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
+                <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名" v-model="name">
               </section>
               <section class="login_verification">
-                <input type="tel" maxlength="8" placeholder="密码">
-                <div class="switch_button off">
-                  <div class="switch_circle"></div>
-                  <span class="switch_text">...</span>
+
+                <input type="text" maxlength="8" placeholder="密码" v-model="pwd" v-if="showPwd">
+                <input type="password" maxlength="8" placeholder="密码" v-model="pwd" v-else>
+
+                <div class="switch_button" :class="showPwd ? 'on' : 'off'" @click="showPwd=!showPwd">
+                  <div class="switch_circle" :class="{right: showPwd}"></div>
+                  <span class="switch_text">{{showPwd?'abc':''}}</span>
                 </div>
               </section>
               <section class="login_message">
-                <input type="text" maxlength="11" placeholder="验证码">
-                <img class="get_verification" src="./images/captcha.svg" alt="captcha">
+                <input type="text" maxlength="11" placeholder="验证码" v-model="captcha">
+                <img class="get_verification" src="http://localhost:3000/captcha" alt="captcha"
+                     @click="updateCaptcha">
               </section>
             </section>
           </div>
@@ -53,13 +60,45 @@
 </template>
 
 <script>
-    export default {}
+  export default {
+    data () {
+      return {
+        loginWay: false, // true代表短信登录, false代表密码登录
+        phone: '', // 手机号
+        code: '', // 短信验证码
+        name: '', // 用户名
+        pwd: '', // 密码
+        captcha: '', // 图片验证码
+        computeTime: 0, // 倒计时的时间
+        showPwd: false // 是否显示密码, 默认不显示
+      }
+    },
+    computed: {
+      // 判断是否是合法的手机号
+      rightPhone () { // 以1开头, 11位数字
+        return /^1\d{10}$/.test(this.phone)
+      }
+    },
+    methods: {
+      sendCode () {
+        this.computeTime = 30;
+        const intervalId = setInterval(() => {
+          this.computeTime--;
+          if(this.computeTime===0) {
+            clearInterval(intervalId)
+          }
+        }, 1000)
+      },
+      updateCaptcha (event) {
+        event.target.src='http://localhost:3000/captcha?time='+Date.now()
+      }
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
   @import "../../common/stylus/mixins.styl"
   .loginContainer
-    position: absolute;
     width 100%
     height 100%
     background #fff
@@ -71,7 +110,7 @@
         .login_logo
           font-size 40px
           font-weight bold
-          color #8bc34a
+          color #02a774
           text-align center
         .login_header_title
           padding-top 40px
@@ -118,6 +157,8 @@
                 color #ccc
                 font-size 14px
                 background transparent
+                &.right_phone_number
+                  color black
             .login_verification
               position relative
               margin-top 16px
@@ -146,7 +187,6 @@
                 &.on
                   background #02a774
                 >.switch_circle
-                  //transform translateX(27px)
                   position absolute
                   top -1px
                   left -1px
@@ -157,6 +197,8 @@
                   background #fff
                   box-shadow 0 2px 4px 0 rgba(0,0,0,.1)
                   transition transform .3s
+                  &.right
+                    transform translateX(26px)
             .login_hint
               margin-top 12px
               color #999
